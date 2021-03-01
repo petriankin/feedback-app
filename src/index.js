8d83e4858a0cd8c59fd21168b6f95736fd39877d
sergei_petriankin <Sergei_petriankin@epam.com>
2021-03-01 22:19:39 +0300

    all, average, positive metrics added

--- src/index.js
+++ src/index.js
@@ -10,17 +10,40 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
+
+    const handleGoodClick = () => {
+        setGood(good + 1);
+        setAll(all + 1);
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1);
+        setAll(all + 1);
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1);
+        setAll(all + 1);
+    }
+
+    const calculateAverage = () => (good - bad) / (good + bad + neutral)
+
+    const calculatePositive = () => (good / all) * 100
 
     return (
         <div>
             <h2>give feedback</h2>
-            <Button handleClick={() => setGood(good + 1)} text="good"/>
-            <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
-            <Button handleClick={() => setBad(bad + 1)} text="bad"/>
+            <Button handleClick={handleGoodClick} text="good"/>
+            <Button handleClick={handleNeutralClick} text="neutral"/>
+            <Button handleClick={handleBadClick} text="bad"/>
             <h2>statistics</h2>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {calculateAverage()}</p>
+            <p>positive {calculatePositive()}</p>
         </div>
     )
 }
